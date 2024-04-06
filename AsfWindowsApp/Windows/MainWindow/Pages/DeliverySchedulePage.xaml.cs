eed6684c5c17468cc770b4652f070da9185656e2
lopata29435 <lopata29435@gmail.com>
2024-04-06 17:00:05 +0300

    Registration sync 002

---
+++ AsfWindowsApp/Windows/MainWindow/Pages/DeliverySchedulePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsfWindowsApp.Windows.MainWindow.Pages
+{
+	/// <summary>
+	/// Логика взаимодействия для DeliverySchedulePage.xaml
+	/// </summary>
+	public partial class DeliverySchedulePage : Page
+	{
+		public DeliverySchedulePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
