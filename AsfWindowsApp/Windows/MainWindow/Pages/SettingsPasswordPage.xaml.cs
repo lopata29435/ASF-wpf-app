812eb6ab5287040b15f369a0065d7d7edec9d95f
supikashi <147439314+supikashi@users.noreply.github.com>
2024-04-22 22:22:39 +0300

    change password pre-release

--- AsfWindowsApp/Windows/MainWindow/Pages/SettingsPasswordPage.xaml.cs
+++ AsfWindowsApp/Windows/MainWindow/Pages/SettingsPasswordPage.xaml.cs
@@ -21,7 +21,7 @@ namespace AsfWindowsApp.Windows.MainWindow.Pages
 	/// </summary>
 	public partial class SettingsPasswordPage : Page
 	{
-		string token = "2b34d859415eea9e3e7453f7306e3e71e9140487c371818c4ba0d753a8ab9210";
+		string token = "394f657232f931de37e3c26626b9ef57da0262f38a046f501bb2ddf6e7dbb192";
 		public SettingsPasswordPage()
 		{
 			InitializeComponent();
@@ -32,6 +32,12 @@ namespace AsfWindowsApp.Windows.MainWindow.Pages
 			string new_password = NewPassword.Text;
 			string confirm_password = ConfirmPassword.Text;
 
+			/*if (new_password != confirm_password)
+			{
+				ConfirmPassword.Style = (Style)FindResource("TextBox.Rectangle.small-error");
+				ConfirmErrorText.Visibility = Visibility.Visible;
+			}*/
+			//if (new_password.Count() < 8 || !new_password.Any(x => 'A' <= x && x <= 'Z') || !new_password.Any(x => '0' <= x && x <= '9') || !new_password.Any(x => '0' <= x && x <= '9'))
 
 			HttpClient client = new HttpClient();
 
@@ -43,18 +49,30 @@ namespace AsfWindowsApp.Windows.MainWindow.Pages
 			};
 			try
 			{
+				Save_Button.IsEnabled = false;
 				HttpResponseMessage response = await client.SendAsync(request);
+				string body = await response.Content.ReadAsStringAsync();
 				Text1.Text = response.StatusCode.ToString();
-				Text2.Text = await response.Content.ReadAsStringAsync();
+				Text2.Text = body;
 				if (response.IsSuccessStatusCode)
 				{
 					NavigationService.Navigate(new SettingsPage());
 				}
 				else
 				{
-					Save_Button.IsEnabled = false;
-					Password.Style = (Style)FindResource("TextBox.Rectangle.small-error");
-					ErrorText.Visibility = Visibility.Visible;
+					if (body == "{\"non_field_errors\":[\"Incorrect password\"]}")
+					{
+						Password.Style = (Style)FindResource("TextBox.Rectangle.small-error");
+						ErrorText.Visibility = Visibility.Visible;
+					} else if (body == "{\"non_field_errors\":[\"Passwords do not match\"]}")
+					{
+						ConfirmPassword.Style = (Style)FindResource("TextBox.Rectangle.small-error");
+						ConfirmErrorText.Visibility = Visibility.Visible;
+					} else
+					{
+						NewPassword.Style = (Style)FindResource("TextBox.Rectangle.small-error");
+						NewErrorText.Visibility = Visibility.Visible;
+					}
 				}
 			}
 			catch (Exception ex)
@@ -69,12 +87,23 @@ namespace AsfWindowsApp.Windows.MainWindow.Pages
 
 		private void Input_TextChanged(object sender, TextChangedEventArgs e)
 		{
-			Save_Button.IsEnabled = Password.Text != "";
-			if (Password.Style == (Style)FindResource("TextBox.Rectangle.small-error"))
+			if (((TextBox)sender).Name == "Password" && Password.Style == (Style)FindResource("TextBox.Rectangle.small-error"))
 			{
 				Password.Style = (Style)FindResource("TextBox.Rectangle.small");
 				ErrorText.Visibility = Visibility.Hidden;
+			} else if (((TextBox)sender).Name == "ConfirmPassword" && ConfirmPassword.Style == (Style)FindResource("TextBox.Rectangle.small-error"))
+			{
+				ConfirmPassword.Style = (Style)FindResource("TextBox.Rectangle.small");
+				ConfirmErrorText.Visibility = Visibility.Hidden;
+			} else if (((TextBox)sender).Name == "NewPassword" && NewPassword.Style == (Style)FindResource("TextBox.Rectangle.small-error"))
+			{
+				NewPassword.Style = (Style)FindResource("TextBox.Rectangle.small");
+				NewErrorText.Visibility = Visibility.Hidden;
 			}
+			Save_Button.IsEnabled = Password.Text != "" && NewPassword.Text != "" && ConfirmPassword.Text != ""
+				&& Password.Style == (Style)FindResource("TextBox.Rectangle.small")
+				&& NewPassword.Style == (Style)FindResource("TextBox.Rectangle.small")
+				&& ConfirmPassword.Style == (Style)FindResource("TextBox.Rectangle.small");
 		}
 	}
 }
