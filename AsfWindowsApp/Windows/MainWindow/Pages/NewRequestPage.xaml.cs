c9ef74e622b8a56b8623b57385e6c849927ecb8a
supikashi <147439314+supikashi@users.noreply.github.com>
2024-05-13 20:59:16 +0300

    Requests alfa

---
+++ AsfWindowsApp/Windows/MainWindow/Pages/NewRequestPage.xaml.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsfWindowsApp.Windows.MainWindow.Pages
+{
+	/// <summary>
+	/// Логика взаимодействия для NewRequestPage.xaml
+	/// </summary>
+	public partial class NewRequestPage : Page
+	{
+		string token = "aad17cda65915d890d27ba38c9129ca4926944cbda6644ad6d883f2088c72e47";
+		OpenFileDialog openFileDialog1 = null;
+		public NewRequestPage()
+		{
+			InitializeComponent();
+			
+		}
+		private void Projects_Click(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new ProjectsPage());
+		}
+		private void Schedule_Click(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new DeliverySchedulePage());
+		}
+		private void Requests_Click(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new RequestsPage());
+		}
+		private void Settings_Click(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new SettingsPage());
+		}
+
+		private void Back_Click(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new RequestsPage());
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			openFileDialog1 = new OpenFileDialog();
+			openFileDialog1.Title = "Выберите файл";
+			openFileDialog1.Filter = "*.pdf;*.doc;*.docx|*.pdf;*.doc;*.docx"; // можно разделить
+
+			bool? result = openFileDialog1.ShowDialog();
+			((Button)sender).Content = openFileDialog1.FileName.Length == 0 ? "Выберете файл" : System.IO.Path.GetFileName(openFileDialog1.FileName);
+			Save_Button.IsEnabled = IsSaveButtonActive();
+		}
+		private async void Save_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				using (var client = new HttpClient())
+				using (var formData = new MultipartFormDataContent())
+				using (var fileStream = File.OpenRead(openFileDialog1.FileName))
+				{
+					client.DefaultRequestHeaders.Add("Authorization", $"Token {token}");
+					// Добавление параметров запроса
+					formData.Add(new StreamContent(fileStream), "document", System.IO.Path.GetFileName(openFileDialog1.FileName));
+					formData.Add(new StringContent(Request.Text), "request");
+					formData.Add(new StringContent(Resolution.Text), "resolution");
+					formData.Add(new StringContent(Number.Text), "specification_number");
+					formData.Add(new StringContent(Addressee.Text), "addressee");
+
+					// Отправка запроса
+					var response = await client.PostAsync($"{Values.Route.ENDPOINT}request/createrequest/", formData);
+
+					if (response.IsSuccessStatusCode)
+					{
+						MessageBox.Show("Документ успешно отправлен на сервер.");
+					}
+					else
+					{
+						MessageBox.Show(await response.Content.ReadAsStringAsync());
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"An error occurred: {ex.Message}");
+			}
+		}
+
+		private bool IsSaveButtonActive()
+		{
+			if (Number.Text.Length == 0) return false;
+			if (Addressee.Text.Length == 0) return false;
+			if (Request.Text.Length == 0) return false;
+			if (Resolution.Text.Length == 0) return false;
+			if (openFileDialog1 == null || openFileDialog1.FileName.Length == 0) return false;
+			return true;
+		}
+
+		private void Number_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			Save_Button.IsEnabled = IsSaveButtonActive();
+		}
+
+		private void Addressee_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			Save_Button.IsEnabled = IsSaveButtonActive();
+		}
+
+		private void Request_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			Save_Button.IsEnabled = IsSaveButtonActive();
+		}
+
+		private void Resolution_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			Save_Button.IsEnabled = IsSaveButtonActive();
+		}
+	}
+}
--- AsfWindowsApp/Windows/MainWindow/Pages/RequestsPage.xaml.cs
+++ AsfWindowsApp/Windows/MainWindow/Pages/RequestsPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,12 +19,142 @@ using System.Windows.Shapes;
 
 namespace AsfWindowsApp.Windows.MainWindow.Pages
 {
+	public class RequestBlock
+	{
+		[JsonProperty("document")]
+		public string Document { get; set; }
+
+		[JsonProperty("request")]
+		public string Request { get; set; }
+
+		[JsonProperty("resolution")]
+		public string Resolution { get; set; }
+
+		[JsonProperty("creator")]
+		public int Creator { get; set; }
+
+		[JsonProperty("specification_number")]
+		public int SpecificationNumber { get; set; }
+
+		[JsonProperty("addressee")]
+		public int Addressee { get; set; }
+	}
 	public partial class RequestsPage : Page
 	{
+		const string token = "aad17cda65915d890d27ba38c9129ca4926944cbda6644ad6d883f2088c72e47";
 		public RequestsPage()
 		{
-			InitializeComponent(); 
+			InitializeComponent();
+			Loaded += RequestsPage_Loaded;
 		}
+		private async void RequestsPage_Loaded(object sender, RoutedEventArgs e)
+		{
+			await LoadRequestsAsync(); // убрать тест
+		}
+		private async Task LoadRequestsAsync()
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				string ApiUrl = $"{Values.Route.ENDPOINT}request/allrequests/";
+				try
+				{
+					var request = new HttpRequestMessage(HttpMethod.Get, ApiUrl);
+					request.Headers.Add("Authorization", $"Token {token}");
+					var response = await client.SendAsync(request);
+
+					if (response.IsSuccessStatusCode)
+					{
+						var json = await response.Content.ReadAsStringAsync();
+						var blocks = JsonConvert.DeserializeObject<List<RequestBlock>>(json);
+						SuccesRequestsLoad(blocks);
+					}
+					else
+					{
+						ErrorRequestsLoad();
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show($"An error occurred: {ex.Message}");
+				}
+			}
+		}
+		private async Task LoadRequestsAsyncTest()
+		{
+			var json = File.ReadAllText("test.txt");
+			var blocks = JsonConvert.DeserializeObject<List<RequestBlock>>(json);
+			SuccesRequestsLoad(blocks);
+		}
+		private void SuccesRequestsLoad(List<RequestBlock> blocks)
+		{
+			int i = 1;
+			foreach (RequestBlock block in blocks)
+			{
+				Border border = new Border();
+				mainStackPanel.Children.Add(border);
+				border.Height = 257;
+				border.CornerRadius = new CornerRadius(16);
+				border.BorderThickness = new Thickness(1);
+				border.BorderBrush = (SolidColorBrush)FindResource("Color.Black-200");
+				border.Margin = new Thickness(0, 8, 0, 0);
+				border.Background = (SolidColorBrush)FindResource("Color.White");
+
+				StackPanel stackPanel = new StackPanel();
+				border.Child = stackPanel;
+				stackPanel.Margin = new Thickness(24, 16, 24, 16);
+
+				TextBlock textBlock = new TextBlock();
+				stackPanel.Children.Add(textBlock);
+				textBlock.Text = $"Запрос №{i}";
+				textBlock.Style = (Style)FindResource("H3Style");
+
+				textBlock = new TextBlock();
+				stackPanel.Children.Add(textBlock);
+				textBlock.Text = $"Создатель: {block.Creator}";
+				textBlock.Style = (Style)FindResource("InpLabelStyle");
+
+				textBlock = new TextBlock();
+				stackPanel.Children.Add(textBlock);
+				textBlock.Text = $"Адресат: {block.Addressee}";
+				textBlock.Style = (Style)FindResource("InpLabelStyle");
+
+				textBlock = new TextBlock();
+				stackPanel.Children.Add(textBlock);
+				textBlock.Text = $"Документ: {block.Document}";
+				textBlock.Style = (Style)FindResource("InpLabelStyle");
+
+				textBlock = new TextBlock();
+				stackPanel.Children.Add(textBlock);
+				textBlock.Text = "Текст запроса:";
+				textBlock.Style = (Style)FindResource("InpLabelStyle");
+				textBlock.FontWeight = FontWeights.Bold;
+				textBlock.Margin = new Thickness(0, 16, 0, 0);
+
+				textBlock = new TextBlock();
+				stackPanel.Children.Add(textBlock);
+				textBlock.Text = block.Request;
+				textBlock.Style = (Style)FindResource("InpLabelStyle");
+
+				textBlock = new TextBlock();
+				stackPanel.Children.Add(textBlock);
+				textBlock.Text = "Резолюция:";
+				textBlock.Style = (Style)FindResource("InpLabelStyle");
+				textBlock.FontWeight = FontWeights.Bold;
+				textBlock.Margin = new Thickness(0, 16, 0, 0);
+
+				textBlock = new TextBlock();
+				stackPanel.Children.Add(textBlock);
+				textBlock.Text = block.Resolution;
+				textBlock.Style = (Style)FindResource("InpLabelStyle");
+
+				i++;
+			}
+		}
+		private void ErrorRequestsLoad()
+		{
+			MessageBox.Show("Dupa:");
+		}
+
 		private void Projects_Click(object sender, RoutedEventArgs e)
 		{
 			NavigationService.Navigate(new ProjectsPage());
@@ -38,6 +172,10 @@ namespace AsfWindowsApp.Windows.MainWindow.Pages
 			NavigationService.Navigate(new SettingsPage());
 		}
 
+		private void NewRequest_Click(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new NewRequestPage());
+		}
 	}
 }
 
